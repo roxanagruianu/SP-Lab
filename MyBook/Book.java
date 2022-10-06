69f723c480e59bed0a72d61e229d30a4a1c72503
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-06 12:38:23 +0300

    Add files via upload

---
+++ MyBook/Book.java
@@ -0,0 +1,7 @@
+public class Book {
+    List<String> paragraph = new ArrayList<String>();
+    List<String> image = new ArrayList<String>();
+    List<String> table = new ArrayList<String>();
+
+
+}
