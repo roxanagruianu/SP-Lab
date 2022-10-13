aa210996ea4123c5ff4f7dd78d7f7c63eb03c0c1
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-13 12:35:58 +0300

    Update Book.java

--- MyBook/Book.java
+++ MyBook/Book.java
@@ -2,29 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
-	String title;
-	List<String> paragraphs = new ArrayList<String>();
-	List<String> images = new ArrayList<String>();
-	List<String> tables = new ArrayList<String>();
-	List<String> contents = new ArrayList<String>();
-	public Book(String title) {
-		this.title = title;
-	}
-	public void createNewParagraph(String paragraph) {
-		paragraphs.add(paragraph);
-		contents.add(paragraph);
-	}
-	public void createNewImage(String image) {
-		images.add(image);
-		contents.add(image);
-	}
-	public void createNewTable(String table) {
-		tables.add(table);
-		contents.add(table);
-	}
-	public void print() {
-		for(int i=0; i<contents.size(); i++) {
-			System.out.print(contents.get(i)+" ");
-		}
-	}
+    String title;
+    Author author;
+    List<Chapter> chapters = new ArrayList<Chapter>();
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public void addAuthor(Author a) {
+        this.author = a;
+    }
+    public int createChapter(String title){
+        Chapter chap = new Chapter(title);
+        subchapters.add(chap);
+        return chapters.size()-1;
+    }
+
+    public Chapter getChapter(int index){
+        return chapters.get(index);
+    }
+
 }
