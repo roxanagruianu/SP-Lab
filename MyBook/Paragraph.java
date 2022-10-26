6682e8a0570c44f06e8504cb7a10670ed4f65552
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-26 22:42:15 +0300

    Update Paragraph.java

--- MyBook/Paragraph.java
+++ MyBook/Paragraph.java
@@ -1,7 +1,15 @@
-public class Paragraph {
-    String text;
 
-    public void print(){
-        System.out.println("Paragraph: "+ text);
-    }
+public class Paragraph extends Element {
+	String text;
+
+	public Paragraph(String text) {
+		super();
+		this.text = text;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Paragraph: " + text);		
+	}
+	
 }
