5520d41236dbdb46621d043b83eb76bbb1ebab61
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-27 08:54:16 +0300

    Update Author.java

--- MyBook/Author.java
+++ MyBook/Author.java
@@ -1,14 +1,15 @@
 
 public class Author {
 	String name;
-	Book book;
-	
-	public Author(String name) {
+	String surname;
+
+	public Author(String name, String surname) {
 		super();
 		this.name = name;
+		this.surname = surname;
 	}
 
 	public void print() {
-		System.out.println("Author: " + name);
+		System.out.println("Author: " + name + surname);
 	}
 }
