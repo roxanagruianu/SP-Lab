00448bb9273c706c1b4b1b4ee132ded996a3d354
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-27 08:55:51 +0300

    Update Book.java

--- MyBook/Book.java
+++ MyBook/Book.java
@@ -1,38 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book {
+public class Book extends Section{
 	String title;
 	List<Author> authors = new ArrayList<>();
-	List<Chapter> chapters = new ArrayList<>();
+	List<Element> content = new ArrayList<>();
 		
 	public Book(String title) {
-		super();
+		super(title);
 		this.title = title;
 	}
 	
-	public int createChapter(String name) {
-		Chapter c = new Chapter(name);
-		chapters.add(c);
-		return chapters.indexOf(c);
-	}
-	
-	public Chapter getChapter(int index) {
-		return chapters.get(index);
-	}
-
 	public void print() {
 		System.out.println("Book: " + title);
+		System.out.println();
 		System.out.println("Authors: ");
 		authors.forEach((a) ->{
 			a.print();
 		});
-		chapters.forEach((c) ->{
+		System.out.println();
+		content.forEach((c) -> {
 			c.print();
 		});
 	}
 	
 	public void addAuthor(Author a) {
 		authors.add(a);
+	}
+	
+	public void addContent(Element e) {
+		content.add(e);
 	}
 }
