71aca96d9d737790a364dc4632b5151111dc712d
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-27 08:54:58 +0300

    Add files via upload

---
+++ MyBook/Section.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Section implements Element{
+	String title;
+	List<Element> elements = new ArrayList<>();
+	
+	public Section(String title) {
+		super();
+		this.title = title;
+	}
+
+	@Override
+	public void print() {
+		System.out.println(title);
+		elements.forEach((e) -> {
+			e.print();
+		});
+	}
+
+	@Override
+	public void add(Element e) {
+		elements.add(e);	
+	}
+
+	@Override
+	public void remove(Element e) {
+		int index = elements.indexOf(e);
+		elements.remove(index);
+	}
+
+	@Override
+	public Element get(int i) {
+		return elements.get(i);
+	}
+		
+}
---
+++ MyBook/TableOfContents.java
@@ -0,0 +1,28 @@
+
+public class TableOfContents implements Element{
+	
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
