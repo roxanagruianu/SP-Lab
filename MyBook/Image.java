1f54919ea6f4e844d1d3db14faa24bd15b1c4a9a
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-10-27 08:56:40 +0300

    Update Image.java

--- MyBook/Image.java
+++ MyBook/Image.java
@@ -1,15 +1,33 @@
 
-public class Image extends Element{
-	String imageName;
+public class Image implements Element{
+	String url;
 	
 	public Image(String imageName) {
 		super();
-		this.imageName = imageName;
+		this.url = imageName;
 	}
 
 	@Override
 	public void print() {
-		System.out.println("Image with name: " + imageName);
+		System.out.println("Image with name: " + url);
+	}
+
+	@Override
+	public void add(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Element e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element get(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
