1070ed8632a81fa06719ce2a5daf147663cca124
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-11-10 12:40:15 +0200

    Update Main.java

--- MyBook/Main.java
+++ MyBook/Main.java
@@ -9,15 +9,12 @@ public class Main {
 		cap1.add(p3);
 		Paragraph p4 = new Paragraph("Paragraph 4");
 		cap1.add(p4);
-		System.out.println("Printing without Alignment");
-		System.out.println();
-		cap1.print();
-		p1.setAlignStrategy(new AlignCenter());
-		p2.setAlignStrategy(new AlignRight());
-		p3.setAlignStrategy(new AlignLeft());
-		System.out.println();
-		System.out.println("Printing with Alignment");
-		System.out.println();
-		cap1.print();
+		cap1.add(new ImageProxy("ImageOne"));
+		cap1.add(new Image("ImageTwo"));
+		cap1.add(new Paragraph("Some text"));
+		cap1.add(new Table("Table 1"));
+		BookStatistics stats = new BookStatistics();
+		cap1.accept(stats);
+		stats.printStatistics();
 	}
 }
