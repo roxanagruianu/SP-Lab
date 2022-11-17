307cbb4005e0db447cb033588d99315cecf2de83
roxanagruianu <92626837+roxanagruianu@users.noreply.github.com>
2022-11-17 12:27:36 +0200

    Update TableOfContents.java

--- MyBook/TableOfContents.java
+++ MyBook/TableOfContents.java
@@ -1,32 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class TableOfContents implements Element, Visitee{
 	
+	List<Element> tocontent = new ArrayList<>();
+	List<Integer> pages = new ArrayList<>();
+	
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		System.out.println("Table of contents: ");
+		for(int i=0; i<tocontent.size();i++) {
+			System.out.println(tocontent.get(i) + " .... " + pages.get(i));
+		}
+	}
+	
+	public void add(Element e, int pag) {
+		tocontent.add(e);
+		pages.add(pag);
+	}
+
+	@Override
+	public void remove(Element e) {
 		
+	}
+
+	@Override
+	public Element get(int i) {
+		return null;
+	}
+
+	public void accept(Visitor visitor) {
+		visitor.visitTableOfContents(this);
 	}
 
 	@Override
 	public void add(Element e) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public void remove(Element e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Element get(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void accept(Visitor visitor) {
-		visitor.visitTableOfContents(this);
 	}
 	
 }
